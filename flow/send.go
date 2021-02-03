558b260ee74b603e6dab0b8ca21aac7427a4f91a
Max Daunarovich <max.daunarovich@gmail.com>
2021-02-04 02:57:20 +0300

    Refactor code to put all account related login for signers inside SendTransaction

--- flow/send.go
+++ flow/send.go
@@ -21,64 +21,174 @@ package cli
 import (
 	"context"
 	"fmt"
-
-	"github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go-sdk/client"
 	"google.golang.org/grpc"
+	"os"
+	"strings"
+
+	"github.com/onflow/flow-go-sdk"
 )
 
-func SendTransaction(host string, serviceAccount *Account, signers []*Account, tx *flow.Transaction, withResults bool) {
+func SendTransaction(projectConf *Config, tx *flow.Transaction, host string, signers string, withResults bool) {
 	ctx := context.Background()
+	host = projectConf.HostWithOverride(host)
 
 	flowClient, err := client.New(host, grpc.WithInsecure())
 	if err != nil {
 		Exitf(1, "Failed to connect to host: %s", err)
 	}
 
+	// Get Service Account and validate it's keys
+	serviceAccount := projectConf.Accounts[serviceAccountName]
+	validateKeyPreReq(serviceAccount)
+
+	argsList := strings.ReplaceAll(signers, " ", "")
+	addresses := strings.Split(argsList, "|")
+
 	signerAccount := serviceAccount
-	if len(signers) > 0 {
-		signerAccount = signers[0]
-	}
+	for i, address := range addresses {
+		account := projectConf.Accounts[address]
 
-	signerAddress := serviceAccount.Address
-
-	fmt.Printf("Getting information for account with address 0x%s ...\n", signerAddress.Hex())
-
-	account, err := flowClient.GetAccount(ctx, signerAddress)
-	if err != nil {
-		Exitf(1, "Failed to get account with address %s: 0x%s", signerAddress.Hex(), err)
-	}
-
-	// Default 0, i.e. first key
-	accountKey := account.Keys[signerAccount.KeyIndex]
-
-	sealed, err := flowClient.GetLatestBlockHeader(ctx, true)
-	if err != nil {
-		Exitf(1, "Failed to get latest sealed block: %s", err)
-	}
-
-	tx.SetReferenceBlockID(sealed.ID).
-		SetProposalKey(signerAddress, accountKey.Index, accountKey.SequenceNumber).
-		SetPayer(signerAddress)
-
-	err = tx.SignEnvelope(signerAddress, accountKey.Index, signerAccount.Signer)
-	if err != nil {
-		Exitf(1, "Failed to sign transaction: %s", err)
-	}
-
-	fmt.Printf("Submitting transaction with ID %s ...\n", tx.ID())
-
-	err = flowClient.SendTransaction(context.Background(), *tx)
-	if err == nil {
-		fmt.Printf("Successfully submitted transaction with ID %s\n", tx.ID())
-	} else {
-		Exitf(1, "Failed to submit transaction: %s", err)
-	}
-	if withResults {
-		res, err := waitForSeal(ctx, flowClient, tx.ID())
-		if err != nil {
-			Exitf(1, "Failed to seal transaction: %s", err)
+		if account != nil {
+			validateKeyPreReq(account)
+		} else {
+			// TODO: check address, if it's not Flow Address - quit app
+			fmt.Printf("Getting information for account with address 0x%s ...\n", WithPrefix(address).Hex())
+			account = signerAccount
 		}
-		printTxResult(tx, res, true)
+
+		// flowAddress := flow.BytesToAddress([]byte(address))
+		// TODO: we need to make sure we are passing specified address and not service account again...
+		tx.AddAuthorizer(account.Address)
+
+		if i == 0 {
+			// TODO: first key should sign the envelope
+			signerAccount = account
+			flowAccount, err := flowClient.GetAccount(ctx, signerAccount.Address)
+			if err != nil {
+				Exitf(1, "Failed to get account with address %s: 0x%s", signerAddress.Hex(), err)
+			}
+
+			accountKey := flowAccount.Keys[signerAccount.KeyIndex]
+
+			sealed, err := flowClient.GetLatestBlockHeader(ctx, true)
+			if err != nil {
+				Exitf(1, "Failed to get latest sealed block: %s", err)
+			}
+
+			tx.
+				SetReferenceBlockID(sealed.ID).
+				SetProposalKey(account.Address, accountKey.Index, accountKey.SequenceNumber).
+				SetPayer(signerAccount.Address)
+		}
+
+		tx.SignPayload()
 	}
+
+	/*	// Signers
+		var accounts []*Account
+
+		argsList := strings.ReplaceAll(signers, " ", "")
+		addresses := strings.Split(argsList, "|")
+
+		for _, address := range addresses {
+
+
+			type Account struct {
+				KeyType    KeyType
+				KeyIndex   int
+				KeyContext map[string]string
+				Address    flow.Address
+				PrivateKey crypto.PrivateKey
+				SigAlgo    crypto.SignatureAlgorithm
+				HashAlgo   crypto.HashAlgorithm
+				Signer     crypto.Signer
+			}
+
+
+			var signerAccount *Account
+			if address == "service" {
+				signerAccount = projectConf.Accounts[address]
+			} else {
+				flowAddress := flow.BytesToAddress([]byte(address))
+				hexAddress := flowAddress.Hex()
+
+				fmt.Printf("Getting information for account with address 0x%s ...\n", hexAddress)
+				signerAccount = GetAccount(host, flowAddress)
+				if err != nil {
+					Exitf(1, "Failed to get account with address %s: 0x%s", hexAddress, err)
+				}
+			}
+
+			validateKeyPreReq(signerAccount)
+			accounts = append(accounts, signerAccount)
+
+			tx.AddAuthorizer(signerAccount.Address)
+			fmt.Printf("Address added: %s \n", address)
+		}
+
+		// Service account
+		serviceAccount := projectConf.Accounts["service"]
+		validateKeyPreReq(serviceAccount)
+
+		// []*Account
+		signerAccount := serviceAccount
+		if len(signers) > 0 {
+			signerAccount = signers[0]
+		}
+
+		signerAddress := serviceAccount.Address
+
+
+
+		// Default 0, i.e. first key
+		accountKey := account.Keys[signerAccount.KeyIndex]
+
+		sealed, err := flowClient.GetLatestBlockHeader(ctx, true)
+		if err != nil {
+			Exitf(1, "Failed to get latest sealed block: %s", err)
+		}
+
+		tx.SetReferenceBlockID(sealed.ID).
+			SetProposalKey(signerAddress, accountKey.Index, accountKey.SequenceNumber).
+			SetPayer(signerAddress)
+
+		err = tx.SignEnvelope(signerAddress, accountKey.Index, signerAccount.Signer)
+		if err != nil {
+			Exitf(1, "Failed to sign transaction: %s", err)
+		}
+
+		fmt.Printf("Submitting transaction with ID %s ...\n", tx.ID())
+
+		err = flowClient.SendTransaction(context.Background(), *tx)
+		if err == nil {
+			fmt.Printf("Successfully submitted transaction with ID %s\n", tx.ID())
+		} else {
+			Exitf(1, "Failed to submit transaction: %s", err)
+		}
+		if withResults {
+			res, err := waitForSeal(ctx, flowClient, tx.ID())
+			if err != nil {
+				Exitf(1, "Failed to seal transaction: %s", err)
+			}
+			printTxResult(tx, res, true)
+		}*/
+}
+
+func validateKeyPreReq(account *Account) {
+	if account.KeyType == KeyTypeHex {
+		// Always Valid
+		return
+	} else if account.KeyType == KeyTypeKMS {
+		// Check GOOGLE_APPLICATION_CREDENTIALS
+		googleAppCreds := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+		if len(googleAppCreds) == 0 {
+			if len(account.KeyContext["projectId"]) == 0 {
+				Exitf(1, "Could not get GOOGLE_APPLICATION_CREDENTIALS, no google service account json provided but private key type is KMS", account.Address)
+			}
+			GcloudApplicationSignin(account.KeyContext["projectId"])
+		}
+		return
+	}
+	Exitf(1, "Failed to validate %s key for %s", account.KeyType, account.Address)
 }
